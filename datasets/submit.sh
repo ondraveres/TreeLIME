6aa76b9d708d8d3e7b735d75be238deec8e22f81
veresond <veresond@login1.rci.cvut.cz>
2024-01-12 16:43:43 +0100

    update

--- datasets/submit.sh
+++ datasets/submit.sh
@@ -4,9 +4,9 @@ for d in mutagenesis hepatitis deviceid; do
     for t in `ls ../../data/raw/${d}`; do 
         counter=0
         for c in `ls ../../data/raw/${d}/${t} | grep concept`; do 
-            if [ $counter -ge 5 ]; then
-                #exit 0
-            fi
+            # if [ $counter -ge 5 ]; then
+            #     #exit 0
+            # fi
             i=${c##*/}
             i=${c%_concept.jsonl}
             sbatch -J ${d}_${t}_${i} -D ${PWD} slurm.sh $d $t $i
