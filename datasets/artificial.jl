b7f91511f37bbd911d5cb045f4c2038d9c851efa
veresond <veresond@login1.rci.cvut.cz>
2024-01-22 16:34:52 +0100

    no errors

--- datasets/artificial.jl
+++ datasets/artificial.jl
@@ -148,15 +148,15 @@ logsoft_model = @set model.m = Chain(model.m, logsoftmax);
 ###############################################################
 #  Helper functions for explainability
 ###############################################################
-const ci = PrayTools.classindexes(labels);
-ci;
+const ci = PrayTools.classindexes(labels)
+ci
 
-ci;
+ci
 
 function loadclass(k, n=typemax(Int))
     dss = map(s -> extractor(s, store_input=true), sample(samples[ci[k]], min(n, length(ci[k])), replace=false))
     reduce(catobs, dss)
-end;
+end
 
 
 function onlycorrect(dss, i, min_confidence=0)
@@ -165,73 +165,79 @@ function onlycorrect(dss, i, min_confidence=0)
     min_confidence == 0 && return (dss)
     correct = ExplainMill.confidencegap(soft_model, dss, i) .>= min_confidence
     dss[correct[:]]
-end;
+end
 
-Random.seed!(settings.incarnation);
-strain = 2;
-ds = loadclass(strain, 1000);
+Random.seed!(settings.incarnation)
+strain = 2
+ds = loadclass(strain, 1000)
+if false
 
-extractor(samples[10111], store_input=true).metadata;
-i = strain;
-concept_gap = minimum(map(c -> ExplainMill.confidencegap(soft_model, extractor(c), i)[1, 1], concepts));
-sample_gap = minimum(map(c -> ExplainMill.confidencegap(soft_model, extractor(c), i)[1, 1], samples[labels.==2]));
-threshold_gap = 0.5;#floor(0.9 * concept_gap, digits=2)
-# correct = predict(soft_model, ds, [1, 2])
-# argmax(soft_model(ds[1]))
-# soft_model(ds)
-# mean(Flux.onecold(model(ds) .== labels))
-# mean(labels)
-# mean(Flux.onecold(soft_model(ds)) .== labels)
-# mean(correct)
+    extractor(samples[10111], store_input=true).metadata
+    i = strain
+    concept_gap = minimum(map(c -> ExplainMill.confidencegap(soft_model, extractor(c), i)[1, 1], concepts))
+    sample_gap = minimum(map(c -> ExplainMill.confidencegap(soft_model, extractor(c), i)[1, 1], samples[labels.==2]))
+    threshold_gap = 0.2#floor(0.9 * concept_gap, digits=2)
+    # correct = predict(soft_model, ds, [1, 2])
+    # argmax(soft_model(ds[1]))
+    # soft_model(ds)
+    # mean(Flux.onecold(model(ds) .== labels))
+    # mean(labels)
+    # mean(Flux.onecold(soft_model(ds)) .== labels)
+    # mean(correct)
 
 
-# ExplainMill.confidencegap(soft_model, ds, 2)
-correct_ds = onlycorrect(ds, strain, 0.1);
-ds = correct_ds;
-@info "minimum gap on concepts = $(concept_gap) on samples = $(sample_gap)";
+    # ExplainMill.confidencegap(soft_model, ds, 2)
+    correct_ds = onlycorrect(ds, strain, 0.1)
+    ds = correct_ds
+    @info "minimum gap on concepts = $(concept_gap) on samples = $(sample_gap)"
 
-heuristic = [:Flat_HAdd, :Flat_HArr, :Flat_HArrft, :LbyL_HAdd, :LbyL_HArr, :LbyL_HArrft];
-uninformative = [:Flat_Gadd, :Flat_Garr, :Flat_Garrft, :LbyL_Gadd, :LbyL_Garr, :LbyL_Garrft];
-variants = vcat(
-    collect(Iterators.product(["stochastic"], vcat(uninformative, heuristic)))[:],
-    collect(Iterators.product(["grad", "gnn", "gnn2", "banz"], vcat(heuristic)))[:],
-);
-ds = ds[1:min(numobs(ds), 100)];
-function getexplainer(name)
-    if name == "stochastic"
-        return ExplainMill.StochasticExplainer()
-    elseif name == "grad"
-        return ExplainMill.GradExplainer2()
-    elseif name == "gnn"
-        return ExplainMill.GnnExplainer()
-    elseif name == "gnn2"
-        return ExplainMill.GnnExplainer()
-    elseif name == "banz"
-        return ExplainMill.DafExplainer()
-    else
-        error("unknown eplainer $name")
-    end
-end;
-
-PrintTypesTersely.on();
-
-ExplainMill.DafExplainer();
-exdf = DataFrame();
-numobs(ds);
-variants;
-if !isfile(resultsdir("stats_" * model_name))
-    for (name, pruning_method) in variants
-        e = getexplainer(name)
-        @info "explainer $e on $name with $pruning_method"
-        flush(stdout)
-        #addexperiment(DataFrame(), e, ds[1], logsoft_model, i, n, threshold_gap, name, pruning_method, 1, settings, statlayer)
-        for j in 1:numobs(ds)
-            global exdf
-            exdf = addexperiment(exdf, e, ds[j], logsoft_model, 2, 0, threshold_gap, name, pruning_method, j, settings, statlayer)
+    heuristic = [:Flat_HAdd, :Flat_HArr, :Flat_HArrft, :LbyL_HAdd, :LbyL_HArr, :LbyL_HArrft]
+    uninformative = [:Flat_Gadd, :Flat_Garr, :Flat_Garrft, :LbyL_Gadd, :LbyL_Garr, :LbyL_Garrft]
+    variants = vcat(
+        collect(Iterators.product(["stochastic"], vcat(uninformative, heuristic)))[:],
+        collect(Iterators.product(["grad", "gnn", "gnn2", "banz"], vcat(heuristic)))[:],
+    )
+    ds = ds[1:min(numobs(ds), 100)]
+    function getexplainer(name)
+        if name == "stochastic"
+            return ExplainMill.StochasticExplainer()
+        elseif name == "grad"
+            return ExplainMill.GradExplainer2()
+        elseif name == "gnn"
+            return ExplainMill.GnnExplainer()
+        elseif name == "gnn2"
+            return ExplainMill.GnnExplainer()
+        elseif name == "banz"
+            return ExplainMill.DafExplainer()
+        else
+            error("unknown eplainer $name")
         end
-        BSON.@save resultsdir("stats_" * model_name) exdf
     end
-end;
+
+    PrintTypesTersely.on()
+
+    ExplainMill.DafExplainer()
+    exdf = DataFrame()
+    numobs(ds)
+    variants
+    if false
+        if !isfile(resultsdir("stats_" * model_name))
+            for (name, pruning_method) in variants
+                e = getexplainer(name)
+                @info "explainer $e on $name with $pruning_method"
+                flush(stdout)
+                #addexperiment(DataFrame(), e, ds[1], logsoft_model, i, n, threshold_gap, name, pruning_method, 1, settings, statlayer)
+                for j in 1:numobs(ds)
+                    global exdf
+                    exdf = addexperiment(exdf, e, ds[j], logsoft_model, 2, 0, 0.1, name, pruning_method, j, settings, statlayer)
+                end
+                BSON.@save resultsdir("stats_" * model_name) exdf
+            end
+        end
+    end
+end
+
+
 using HierarchicalUtils
 ### the real deal
 function iterate_over(mask, ds, extractor, sch)
@@ -253,18 +259,18 @@ end
 ### the players
 PrintTypesTersely.off()
 
-sample = ds[1]
+mysample = ds[1]
 
-mask = ExplainMill.create_mask_structure(sample, d -> ExplainMill.ParticipationTracker(SimpleMask(ones(Bool, d))))
+mask = ExplainMill.create_mask_structure(mysample, d -> ExplainMill.ParticipationTracker(SimpleMask(ones(Bool, d))))
 
 sch[:lumo]
-sample[:lumo]
+mysample[:lumo]
 extractor[:lumo]
 mask[:lumo]
 
-printtree(sample)
+printtree(mysample)
 
-typeof(sample)
+typeof(mysample)
 
 iterate_over(mask, ds[1], extractor, sch)
 
@@ -279,7 +285,7 @@ ExplainMill.levelbylevelsearch!(f, mask)
 logical = ExplainMill.e2boolean(ds[1], mask, extractor)
 
 
-typeof(Mill.children(sample))
+typeof(Mill.children(mysample))
 
 Mill.children(sch)
 
@@ -293,82 +299,86 @@ extractor[:atoms]
 
 Mill.children(extractor[:atoms].item)
 
-HierarchicalUtils.nleafs(sample)
+HierarchicalUtils.nleafs(mysample)
 
-NodeIterator(sample) |> collect
+NodeIterator(mysample) |> collect
 
-LeafIterator(sample, mask) |> collect
+LeafIterator(mysample, mask) |> collect
 LeafIterator(mask) |> collect
 
-leafmap!(sample, mask, sch, extractor; complete=false, order=LevelOrder()) do (n1, n2, n3, n4)
-    @show n1
-    @show n2
-    @show n3
-    @show n4
+
+leafmap!(mysample, mask, sch, extractor; complete=false, order=LevelOrder()) do (data_node, mask_node, schema_node, extractor_node)
+    # @show data_node
+    @show mask_node.mask
+    @show typeof(mask_node.mask)
+    # @show schema_node
+    # @show extractor_node
 end
 
-dump(ds[1][mask])
-
-nnodes(ds[1])
-values(ds[1])
-ds[1]
-dump(sch)
-extractor
-
-ds[mask]
-
-ExplainMill.
-ds
 
 
+# dump(ds[1][mask])
 
-log.(soft_model(ds[1]))
-logsoft_model(ds[1])
-concepts[1]
-ms = ExplainMill.explain(ExplainMill.StochasticExplainer(), ds[1], model, pruning_method=:Flat_Gadd)
-logical = ExplainMill.e2boolean(ds[1], ms, extractor)
+# nnodes(ds[1])
+# values(ds[1])
+# ds[1]
+# dump(sch)
+# extractor
+
+# ds[mask]
+
+# ExplainMill.
+# ds
 
 
-logical
-PrintTypesTersely.off()
-repr(logical)
-repr(concepts[1])
-concepts[1]
-ce = map(c -> jsondiff(c, logical), concepts)
-ec = map(c -> jsondiff(logical, c), concepts)
-logical
-ds[20][:lumo]
-concepts[1]
-logical
-ec[1]
-ce[1]
-ds[1][ms]
-concepts[1]
-gap = ExplainMill.confidencegap(soft_model, ds[1][ms], 2)
-fv = ExplainMill.FlatView(ms)
-logical = ExplainMill.e2boolean(ds[1], ms, extractor)
-ce = map(c -> jsondiff(c, logical), concepts)
-ec = map(c -> jsondiff(logical, c), concepts)
 
-ce = jsondiff(concepts[1], logical)
-ec = jsondiff(logical, concepts[1])
+# log.(soft_model(ds[1]))
+# logsoft_model(ds[1])
+# concepts[1]
+# ms = ExplainMill.explain(ExplainMill.StochasticExplainer(), ds[1], model, pruning_method=:Flat_Gadd)
+# logical = ExplainMill.e2boolean(ds[1], ms, extractor)
 
-ce = jsondiff(concepts[1], logical)
-ec = jsondiff(logical, concepts[1])
 
-c = concepts[1]
-logical
-c
-ec
-ce
-ds[1]
-logical = ExplainMill.e2boolean(ds[1], ms, extractor)
-logical
-logical
-excess_nodes = nnodes(ec)
-mynnodes = nnodes(logical)
-println("done")
-println("resultsdir() = ", resultsdir())
+# logical
+# PrintTypesTersely.off()
+# repr(logical)
+# repr(concepts[1])
+# concepts[1]
+# ce = map(c -> jsondiff(c, logical), concepts)
+# ec = map(c -> jsondiff(logical, c), concepts)
+# logical
+# ds[20][:lumo]
+# concepts[1]
+# logical
+# ec[1]
+# ce[1]
+# ds[1][ms]
+# concepts[1]
+# gap = ExplainMill.confidencegap(soft_model, ds[1][ms], 2)
+# fv = ExplainMill.FlatView(ms)
+# logical = ExplainMill.e2boolean(ds[1], ms, extractor)
+# ce = map(c -> jsondiff(c, logical), concepts)
+# ec = map(c -> jsondiff(logical, c), concepts)
+
+# ce = jsondiff(concepts[1], logical)
+# ec = jsondiff(logical, concepts[1])
+
+# ce = jsondiff(concepts[1], logical)
+# ec = jsondiff(logical, concepts[1])
+
+# c = concepts[1]
+# logical
+# c
+# ec
+# ce
+# ds[1]
+# logical = ExplainMill.e2boolean(ds[1], ms, extractor)
+# logical
+# logical
+# excess_nodes = nnodes(ec)
+# mynnodes = nnodes(logical)
+# println("done")
+# println("resultsdir() = ", resultsdir())
 
 
 
